08fa727a416dd2b6c78e052edaa8cddad9b9b566
MantraGitH <camilo.arias56@hotmail.com>
2023-11-07 20:15:01 -0300

    agregando detalles del desafio 4

---
+++ src/public/index.js
@@ -0,0 +1,15 @@
+const socket = io();
+
+const price = document.getElementById("price");
+const output = document.getElementById("output");
+const actions = document.getElementById("actions");
+
+socket.on("products", (data) => {
+  actions.innerHTML = "";
+  const productsRender = data
+    .map((products) => {
+      return `${products}`;
+    })
+    .join(" ");
+  output.innerHTML = productsRender;
+});
---
+++ src/routers/views.router.js
@@ -0,0 +1,26 @@
+import {Router} from 'express';
+const router = Router();
+import fs from 'fs';
+
+async function loadProducts(){
+    try {
+        const productsData = await fs.promises.readFile('./src/data/products.json', 'utf-8');
+        const productsJSON = JSON.parse(productsData);
+        return productsJSON;
+    } catch (error) {
+        throw new Error (`Error al cargar los productos: ${error.message}`);
+    }
+}
+const products = await loadProducts();
+
+router.get('/home', async (req, res)=>{
+    console.log('console.log 1:', products);
+    res.render('home' , {products: products})
+})
+
+router.get('/realtimeproducts', (req, res)=>{
+    res.render('realTimeProducts')
+})
+
+
+export default router;
